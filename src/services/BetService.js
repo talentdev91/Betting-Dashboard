a9a3144a4091dcaa27e2838940c0782707556fa1
goldsun27 <todayhonest27@gmail.com>
2022-10-17 12:02:16 -0300

    Changing NBA chart color

--- src/services/BetService.js
+++ src/services/BetService.js
@@ -147,7 +147,7 @@ const update_bar_chart_colors = (barChartInfo) => {
 }
 
 const project_colors = [
-    '#9C9EFE', '#6594C0', '#38003c', '#2B4865', '#41b883', '#774360', '#D3010C', '#FF7F3F', '#7DCE13', '#224B0C'
+    '#17408B', '#6594C0', '#38003c', '#2B4865', '#41b883', '#774360', '#D3010C', '#9C9EFE', '#7DCE13', '#224B0C'
 ]
 
 const check_bet_outcome = (bet, generalInfo, barChartInfo, chartInfo) => {
