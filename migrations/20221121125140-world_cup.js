1b96de6f21b46ba125f11a392904cc20acdd1f31
goldsun27 <todayhonest27@gmail.com>
2022-11-21 10:00:06 -0300

    World Cup

---
+++ migrations/20221121125140-world_cup.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.bulkInsert('leagues', [
+            { id: 1111, name: 'World Cup', sportId: 1, createdAt: new Date(), updatedAt: new Date() },
+        ]);
+        const { readFile } = require('fs/promises');
+        let teams = JSON.parse(await readFile(`src/utils/teams/world_cup.json`, "utf8"));
+        let teamsToInsert = [];
+        let leagueTeamsToInsert = [];
+        for (let i = 0; i < teams.length; i++) {
+            const teamCount = (await queryInterface.sequelize.query(`SELECT COUNT(*) FROM teams WHERE id = ${teams[i].team.id}`))[0][0]['COUNT(*)']
+            if (teamCount == 0) {
+                teamsToInsert.push({ id: teams[i].team.id, name: teams[i].team.name, createdAt: new Date(), updatedAt: new Date() });
+            }
+            leagueTeamsToInsert.push({ teamId: teams[i].team.id, leagueId: 1111, season: 2022, createdAt: new Date(), updatedAt: new Date() });
+        }
+        if(teamsToInsert.length) {
+            await queryInterface.bulkInsert('teams', teamsToInsert);
+        }
+        await queryInterface.bulkInsert('leagueTeams', leagueTeamsToInsert);
+    },
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+         */
+    }
+};
